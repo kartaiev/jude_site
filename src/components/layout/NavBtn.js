a9dfd27b62c22cbfb6c530e05e7eb6b3659e3eda
Volodymyr Kartaiev <kartayev@gmail.com>
2019-11-18 22:48:11 +0200

    1st

---
+++ src/components/layout/NavBtn.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Nav-btn.css";
+
+export const NavBtn = () => {
+  return (
+    <div className="nav-btn" onClick={this.handleClick}>
+      <div className="nav-line" />
+      <div className="nav-line" />
+      <div className="nav-line" />
+    </div>
+  );
+};
+
+const handleClick = () => {
+  style = {};
+};
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Navbar.css';
+
+class Navbar extends Component {
+
+  state = {};
+
+  render () {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Navbar;
