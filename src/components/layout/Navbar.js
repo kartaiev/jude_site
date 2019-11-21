765396f49a90401bce8b2286cab1887f10aa77ac
Volodymyr Kartaiev <kartayev@gmail.com>
2019-11-21 16:46:35 +0200

    scrollbar-test

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,17 +1,17 @@
 import React from "react";
-import PerfectScrollbar from "react-perfect-scrollbar";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import "./Navbar.scss";
+import { Scrollbars } from "react-custom-scrollbars";
 
 export const Navbar = ({ classEl, lessons, showEl }) => {
   return (
     <nav className={`navbar ${classEl}`}>
       <FontAwesomeIcon icon={faChevronUp} className="arrow-up" />
       <ul className={`${classEl}`}>
-        <PerfectScrollbar>
+        <Scrollbars autoHide style={{ color: "#405b61", overflowX: "hidden" }}>
           {lessons.map(lesson => (
             <li className={`li-style ${classEl}`} onClick={showEl} key={lesson}>
               <NavLink to={`/${lesson}`} className={`link ${classEl}`}>
@@ -19,7 +19,7 @@ export const Navbar = ({ classEl, lessons, showEl }) => {
               </NavLink>
             </li>
           ))}
-        </PerfectScrollbar>
+        </Scrollbars>
       </ul>
       <FontAwesomeIcon icon={faChevronDown} className="arrow-down" />
     </nav>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import "./assets/fonts/dirty.otf";
+import "./assets/fonts/miama.otf";
+import "./index.scss";
 
 ReactDOM.render(
   <Router>
